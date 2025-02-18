c30c18d134eecaea63478227be210bda99209745
Bhavy Mangukiya <152584677+bhavy207@users.noreply.github.com>
2025-02-18 15:05:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
     next();
 });
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/logs', (req, res) => {
     fs.readFile('visits.log', 'utf8', (err, data) => {
         if (err) {
